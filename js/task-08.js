deb3e48df545e61352b10e95cd5661cfc1255209
Sergey Miniaylo <po4tovuj@gmail.com>
2022-08-16 23:10:36 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '')
+    alert('all fields should be filled');
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+  event.currentTarget.reset();
+}
