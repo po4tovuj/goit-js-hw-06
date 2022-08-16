b267d91819ac8b28e300c1a1f80860929c66d18c
Sergey Miniaylo <po4tovuj@gmail.com>
2022-08-16 23:16:29 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', changeColor);
+function changeColor() {
+  const spanElement = document.querySelector('.color');
+  const bodyEl = document.querySelector('body');
+  const newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanElement.innerHTML = newColor;
+}
